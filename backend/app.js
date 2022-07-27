e0295b04e474016ddc09a7e77c695e1b0a125e92
Hazel Guzzetti <viizionprime@gmail.com>
2022-07-27 16:44:18 -0400

    Added error handlers for Express

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const routes = require("./routes");
 const { environment } = require("./config");
+const { ValidationError } = require("sequelize");
 
 const isProduction = environment === "production";
 console.log(isProduction);
@@ -40,4 +41,34 @@ app.use(
 );
 
 app.use(routes);
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = "Validation error";
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
 module.exports = app;
