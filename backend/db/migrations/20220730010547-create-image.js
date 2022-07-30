5c2c99b140dd7e0aee26a762958690802dc85ca7
Hazel Guzzetti <viizionprime@gmail.com>
2022-07-29 21:09:59 -0400

    Initial model/migration created for Images table.

---
+++ backend/db/migrations/20220730010547-create-image.js
@@ -0,0 +1,48 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Images", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      previewImage: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      spotId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "Spots" },
+      },
+      reviewId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "Reviews" },
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "Users" },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Images");
+  },
+};
