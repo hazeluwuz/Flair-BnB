500e9db3afc17690ae12ae23bdf674b45b110a5a
Hazel Guzzetti <viizionprime@gmail.com>
2022-07-29 22:50:30 -0400

    Adjusted response due to invalid authentication to fit API docs.

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -54,9 +54,8 @@ const restoreUser = (req, res, next) => {
 const requireAuth = function (req, _res, next) {
   if (req.user) return next();
 
-  const err = new Error("Unauthorized");
-  err.title = "Unauthorized";
-  err.errors = ["Unauthorized"];
+  const err = new Error("Authentication required");
+  err.message = "Authentication required";
   err.status = 401;
   return next(err);
 };
