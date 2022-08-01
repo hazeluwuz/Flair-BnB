deffa6fc70163c99e9fa889b848f003165b62577
Hazel Guzzetti <viizionprime@gmail.com>
2022-08-01 16:10:53 -0400

    Added router for reviews, and added GET /reviews/current

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const sessionRouter = require("./session");
 const usersRouter = require("./users");
 const spotsRouter = require("./spots");
+const reviewsRouter = require("./reviews");
 const { restoreUser } = require("../../utils/auth.js");
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
@@ -12,6 +13,7 @@ router.use(restoreUser);
 router.use("/session", sessionRouter);
 router.use("/users", usersRouter);
 router.use("/spots", spotsRouter);
+router.use("/reviews", reviewsRouter);
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
 });
---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,19 @@
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+const { requireAuth } = require("../../utils/auth");
+const { Review, User } = require("../../db/models");
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/current", requireAuth, async (req, res, next) => {
+  const userId = req.user.id;
+  const reviews = Review.findAll({
+    where: {
+      userId,
+    },
+  });
+  res.json({ Reviews: reviews });
+});
+
+module.exports = router;
