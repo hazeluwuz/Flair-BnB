0929b87e12bd9023bf0b5888f279ee3df59efb51
Hazel Guzzetti <viizionprime@gmail.com>
2022-08-01 14:58:56 -0400

    Added validation checks to create-spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,6 +5,39 @@ const { requireAuth } = require("../../utils/auth");
 const { Spot, User, Image } = require("../../db/models");
 const express = require("express");
 const router = express.Router();
+const validateSpotData = [
+  check("address")
+    .exists({ checkFalsy: true })
+    .withMessage("Street address is required"),
+  check("city")
+    .exists({ checkFalsy: true })
+    .withMessage("City is required"),
+  check("state")
+    .exists({ checkFalsy: true })
+    .withMessage("State is required"),
+  check("country")
+    .exists({ checkFalsy: true })
+    .withMessage("Country is required"),
+  check("lat")
+    .exists({ checkFalsy: true })
+    .isDecimal()
+    .withMessage("Latitude is not valid"),
+  check("lng")
+    .exists({checkFalsy:true})
+    .isDecimal()
+    .withMessage("Longitude is not valid"),
+  check("name")
+    .exists({checkFalsy:true})
+    .isLength({ max: 50 })
+    .withMessage("Name must be less than 50 characters"),
+  check("description")
+    .exists({checkFalsy:true})
+    .withMessage("Description is required"),
+   check("price")
+    .exists({checkFalsy:true})
+    .withMessage("Price per day is required"),
+  handleValidationErrors,
+];
 
 router.get("/", async (req, res, next) => {
   // need to add avgReview and previewImage once implemented
@@ -24,7 +57,7 @@ router.get("/current", requireAuth, async (req, res, next) => {
   res.json({ Spots: spots });
 });
 
-router.get("/:spotId", requireAuth, async (req, res, next) => {
+router.get("/:spotId", async (req, res, next) => {
   // must add numReviews, and avgStarRating once implemented.
   const spot = await Spot.findByPk(req.params.spotId, {
     include: [
@@ -46,6 +79,13 @@ router.get("/:spotId", requireAuth, async (req, res, next) => {
   } else {
     res.json(spot);
   }
+});
+
+router.post("/", requireAuth, validateSpotData, async (req, res, next) => {
+  const id = req.user.id;
+  const spotData = Object.assign({ ownerId: id }, req.body);
+  const newSpot = await Spot.create(spotData);
+  res.json(newSpot);
 });
 
 module.exports = router;
