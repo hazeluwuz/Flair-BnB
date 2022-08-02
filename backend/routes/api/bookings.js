ef9b89e348df0911eeb323dc6e5ea9ddbec25850
Hazel Guzzetti <viizionprime@gmail.com>
2022-08-02 15:59:03 -0400

    Initial work towards preventing overlapping bookings.

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -7,8 +7,10 @@ const {
   sequelize,
 } = require("../../db/models");
 const { requireAuth } = require("../../utils/auth");
+const { Op } = require("sequelize");
 
 const express = require("express");
+const { contextsKey } = require("express-validator/src/base");
 const router = express.Router();
 
 router.get("/current", requireAuth, async (req, res, next) => {
@@ -23,6 +25,8 @@ router.get("/current", requireAuth, async (req, res, next) => {
 
 router.put("/:bookingId", requireAuth, async (req, res, next) => {
   const booking = await Booking.findByPk(req.params.bookingId);
+  const { startDate, endDate } = req.body;
+  console.log(startDate,endDate);
   if (!booking) {
     const err = new Error("Booking couldn't be found");
     err.message = "Booking couldn't be found";
@@ -35,6 +39,36 @@ router.put("/:bookingId", requireAuth, async (req, res, next) => {
     err.message = "Forbidden";
     err.status = 403;
     next(err);
+  }
+  const spotId = booking.spotId;
+  const currentSpotBookings = await Booking.findAll({
+    where: {
+      spotId: spotId,
+      [Op.and]: [
+        {
+          startDate: {
+            [Op.lte]: endDate,
+          },
+        },
+        {
+          endDate: {
+            [Op.gte]: startDate,
+          },
+        },
+      ],
+    },
+  });
+  if (currentSpotBookings.length) {
+    const err = new Error(
+      "Sorry, this spot is already booked for the specified dates"
+    );
+    err.status = 403;
+    err.message = "Sorry, this spot is already booked for the specified dates";
+    err.errors = {
+      startDate: "Start date conflicts with an existing booking",
+      endDate: "End date conflicts with an existing booking",
+    };
+    return next(err);
   }
 
   await booking.set(req.body);
