ada851814fd5b1f0e828ab35b078c237234f9773
Hazel Guzzetti <viizionprime@gmail.com>
2022-08-02 15:35:00 -0400

    added route handler to edit booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -21,4 +21,26 @@ router.get("/current", requireAuth, async (req, res, next) => {
   res.json(bookings);
 });
 
+router.put("/:bookingId", requireAuth, async (req, res, next) => {
+  const booking = await Booking.findByPk(req.params.bookingId);
+  if (!booking) {
+    const err = new Error("Booking couldn't be found");
+    err.message = "Booking couldn't be found";
+    err.status = 404;
+    next(err);
+  }
+
+  if (booking.userId !== req.user.id) {
+    const err = new Error("Forbidden");
+    err.message = "Forbidden";
+    err.status = 403;
+    next(err);
+  }
+
+  await booking.set(req.body);
+  await booking.validate();
+  await booking.save();
+  res.json(booking);
+});
+
 module.exports = router;
