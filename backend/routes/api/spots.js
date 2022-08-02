48990350bb0f2f81ebf67f5a308b43710bc50071
Hazel Guzzetti <viizionprime@gmail.com>
2022-08-02 17:46:51 -0400

    potential fix regarding postgress

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -34,17 +34,22 @@ const spotFound = function (spot, next) {
 router.get("/", async (req, res, next) => {
   // need to add avgReview and previewImage once implemented
   const spots = await Spot.findAll({
-    attributes: {
-      include: [
-        [sequelize.fn("AVG", sequelize.col("Reviews.stars")), "avgRating"],
-      ],
-    },
     include: {
       model: Review,
       attributes: [],
-      right: true,
     },
   });
+  for (let spot of spots) {
+    const spotReviewData = await Review.findAll({
+      where: {
+        spotId: spot.id,
+      },
+      attributes: [
+        [sequelize.fn("AVG", sequelize.col("stars")), "avgStarRating"],
+      ],
+    });
+    spot.dataValues.avgRating = spotReviewData[0].dataValues.avgStarRating;
+  }
   res.json({ Spots: spots });
 });
 
