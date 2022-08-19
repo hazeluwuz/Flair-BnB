8b9549bd947339563a4e7612b32906edf149e06c
Hazel Guzzetti <viizionprime@gmail.com>
2022-08-19 13:27:38 -0400

    removed extra console.log

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -19,7 +19,6 @@ function LoginFormPage() {
     return dispatch(sessionActions.login({ credential, password })).catch(
       async (res) => {
         const data = await res.json();
-        console.log(data)
         if (data && data.errors) setErrors(data.errors);
       }
     );
