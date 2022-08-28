0b985b966b321ede95148481e61386d541982d14
Hazel Guzzetti <viizionprime@gmail.com>
2022-08-27 21:00:59 -0400

    adjusted createdAt for debugging

--- backend/db/migrations/20220730005841-create-review.js
+++ backend/db/migrations/20220730005841-create-review.js
@@ -31,12 +31,12 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: new Date(),
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal(),
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
     });
   },
